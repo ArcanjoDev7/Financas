deeac5e322116dfd776d4d86b6fa576832845ac7
ArcanjoDev7 <miguelarcanjodearaujocosta@gmail.com>
2024-06-22 00:20:32 -0300

    dsadsadsadsa

--- Financas.Persistence/Repositories/AccoutRepository.cs
+++ Financas.Persistence/Repositories/AccoutRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace Financas.Infra.Persistence.Repositories
-{
-    public class AccoutRepository
+﻿using Financas.Domain.Models;
+using Financas.Infra.Persistence.Repositories.Interfaces;
+
+namespace Financas.Infra.Persistence.Repositories
     {
+        public class AccoutRepository(AppDbContext context) : RepositoryBase<Account>(context), IAccountRepository
+        {
+            public async Task<Account?> GetByIdAsync(Guid id)
+            {
+                return await Context.Accounts
+                    .FirstOrDefaultAsync(t => t.Id == id);
+            }
+            public async Task<Account?> GetAsync(string name)
+        {
+            return await Context.Accounts
+               .FirstOrDefaultAsync(x => x.Name == name);
+        }
+            public async Task<Account?> CreateAsync(Account account)
+        {
+            await Context.Accounts.AddAsync(account);
+            await Context.SaveChangesAsync();
+            return account;
+        }
     }
 }
